3ceee6a5284860879260aee6b82b981af05d5117
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-31 15:11:09 +0100

    finished mvp

--- js/app.js
+++ js/app.js
@@ -2,11 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
   const form = document.querySelector('#new-item-form');
-  form.addEventListener('submit', handleForm);
+  form.addEventListener('submit', handle);
+  
 
-})
+});
 
-const handleForm = function(event){
+const handle = function(event){
+  event.preventDefault();
   const newList = document.createElement('li');
-  const
+  const newHead1 = document.createElement('h2');
+  newHead1.textContent = `${event.target.title.value}`;
+  console.dir(event)
+  newList.appendChild(newHead1);
+  const newHead2 = document.createElement('h3');
+  newHead2.textContent = `${event.target.author.value}`;
+  newList.appendChild(newHead2);
+  const newPara = document.createElement('p');
+  newPara.textContent = `${event.target.category.value}`;
+  newList.appendChild(newPara);
+  const list = document.querySelector('#reading-list');
+  list.appendChild(newList);
+  const form = document.querySelector('#new-item-form');
+  form.reset();
 }
+
