0b60c66b5b2a15359638846960199d4ba55848e5
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-31 15:32:12 +0100

    mvp

--- js/app.js
+++ js/app.js
@@ -10,19 +10,31 @@ document.addEventListener('DOMContentLoaded', () => {
 const handle = function(event){
   event.preventDefault();
   const newList = document.createElement('li');
-  const newHead1 = document.createElement('h2');
-  newHead1.textContent = `${event.target.title.value}`;
-  console.dir(event)
-  newList.appendChild(newHead1);
-  const newHead2 = document.createElement('h3');
-  newHead2.textContent = `${event.target.author.value}`;
-  newList.appendChild(newHead2);
-  const newPara = document.createElement('p');
-  newPara.textContent = `${event.target.category.value}`;
-  newList.appendChild(newPara);
+  newList.setAttribute('class', 'reading')
+  const valueList = [event.target.title.value, event.target.author.value,  event.target.category.value]
+  for (i=0; i<valueList.length; i++){
+    const newPara = document.createElement('p');
+    newPara.textContent = `${valueList[i]}`;
+    
+    
+    newList.appendChild(newPara);
+  }
   const list = document.querySelector('#reading-list');
   list.appendChild(newList);
   const form = document.querySelector('#new-item-form');
   form.reset();
 }
 
+
+// const newHead1 = document.createElement('h2');
+//   newHead1.textContent = `${event.target.title.value}`;
+//   console.dir(event)
+//   newList.appendChild(newHead1);
+//   const newHead2 = document.createElement('h3');
+//   newHead2.textContent = `${event.target.author.value}`;
+//   newList.appendChild(newHead2);
+//   const newPara = document.createElement('p');
+//   newPara.textContent = `${event.target.category.value}`;
+//   newList.appendChild(newPara);
+  // const list = document.querySelector('#reading-list');
+  // list.appendChild(newList);
