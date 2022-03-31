4e076189c4c6a9aacda41539783dd380fc14998c
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-31 14:33:26 +0100

    first yay

---
+++ js/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForm);
+
+})
+
+const handleForm = function(event){
+  const newList = document.createElement('li');
+  const
+}
